e9b885c9605437c1d39c26428d732dee0193334e
Adam Paz <adampaz8@gmail.com>
2018-11-27 14:32:49 -0800

    wip

--- pythonfn/func.py
+++ pythonfn/func.py
@@ -5,9 +5,9 @@ import requests
 
 def handler(ctx, data=None, loop=None):
  
-    req = requests.get('http://pokeapi.co/api/v2/pokemon/1/')
+    req = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins=Los Angeles&destinations=Irvine&key=AIzaSyD27N9mxT47VEQ3MX80dZZJa4_HdczBd_4')
     json_response = json.loads(req.content)
-    return("Pokemon Name: " + json_response['name'])
+    return("rows: " + json.dumps(json_response['rows'][0]['elements'][0]['duration']['value']))
 
 if __name__ == "__main__":
     fdk.handle(handler)
